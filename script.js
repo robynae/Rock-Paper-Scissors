802f7b87dc79f6bdc5b81e9355eab8061c55993d
robynae <92494880+robynae@users.noreply.github.com>
2023-09-26 17:26:27 -0500

    Create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerSelection;
+let computerSelection;
+let playerScore;
+let computerScore;
+
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3) + 1;
 
@@ -9,3 +14,25 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    } else if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+     ) {
+        return `Congrats you win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`
+     } else if (
+        (playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock')
+     ) {
+        return `Sorry, you lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.}`
+     } else {
+        return "I'm not sure what you've entered."
+     }
+} 
