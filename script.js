e8e92cd5d370c6b92b54468ff3e353b57a090c5d
robynae <92494880+robynae@users.noreply.github.com>
2023-09-26 16:39:29 -0500

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3) + 1;
+
+    if (number === 1) {
+        return "Rock";
+    } else if(number === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
