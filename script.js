4ec48fa2d9d16bc22ee2d95d144689c721849476
robynae <92494880+robynae@users.noreply.github.com>
2023-09-27 09:28:07 -0500

    Create game function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let playerSelection;
 let computerSelection;
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3) + 1;
@@ -15,24 +15,43 @@ function getComputerChoice() {
     }
 }
 
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice().toLowerCase();
     if (playerSelection == computerSelection) {
-        return "It's a tie!"
+        return "It's a tie!";
     } else if (
         (playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')
      ) {
-        return `Congrats you win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`
-     } else if (
+        playerScore += 1;
+        return `Congrats you win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}! You have ${playerScore} points and the computer has ${computerScore} points.`
+    } else if (
         (playerSelection == 'rock' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'rock')
      ) {
-        return `Sorry, you lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.}`
+        computerScore += 1;
+        return `Sorry, you lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}. You have ${playerScore} points and the computer has ${computerScore} points.`
      } else {
         return "I'm not sure what you've entered."
      }
 } 
+
+//use a for loop instead
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Do you choose rock, paper, or scissors?");
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
+    if (playerScore > computerScore) {
+        return "Hurray! You've won it all!";
+    } else {
+        return "You've lost. Better luck next time."
+    }
+    playerScore = 0;
+    computerScore = 0;
+}
